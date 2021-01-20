fef013442ef94e364c369af57a6b3cb3d856be4c
JuanSoulAxel <barruan.mm@gmail.com>
2021-01-20 10:30:58 -0300

    Adicionando primeiro projeto ainda em desenvolvimento

---
+++ _scripts/script.js
@@ -0,0 +1,30 @@
+function carregarJS() {
+var data = new Date();
+var diaDaSemana = ["Domingo","Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"];
+var mes = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+var infDate = document.querySelector(`p#infDate`);
+infDate.innerHTML = `${diaDaSemana[data.getDay()]}, ${mes[data.getMonth()]} ${data.getDate()}, ${data.getFullYear()}`;
+
+/*
+var dia     = data.getDate();           // 1-31
+var dia_sem = data.getDay();            // 0-6 (zero=domingo)
+var mes     = data.getMonth();          // 0-11 (zero=janeiro)
+var ano2    = data.getYear();           // 2 dígitos
+var ano4    = data.getFullYear();       // 4 dígitos
+var hora    = data.getHours();          // 0-23
+var min     = data.getMinutes();        // 0-59
+var seg     = data.getSeconds();        // 0-59
+var mseg    = data.getMilliseconds();   // 0-999
+var tz      = data.getTimezoneOffset(); // em minutos
+*/
+}
+
+function entrar() { //função de teste (temporária)
+    var botao = document.querySelector(`nav#menu2`);
+    botao.style.display = "block";
+}
+
+function abrirCadastro() {
+    window.open("cadastro.html");
+}
